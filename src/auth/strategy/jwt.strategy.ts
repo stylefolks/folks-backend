3effc39b75314a2f857ae331ac570735c298cde2
sanghun Lee <sanghunlee@sanghunui-MacBookPro.local>
2025-04-28 22:53:26 +0900

    feat: 토큰 발행, bcrypt, ConfigModule 적용

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'default_secret_key',
+    });
+  }
+
+  /**
+   * @description
+   * 매 요청마다 토큰에서 id 뽑고
+   * DB에서 실제 유저 확인하고
+   * req.user에 유저 정보 세팅
+   * Controller에서는 안전하게 req.user 꺼내서 사용
+   * @param payload
+   * @returns
+   */
+  async validate(payload: { sub: string; email: string }) {
+    const user = await this.userService.findById(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    //req.user에 해당 정보 항상 세팅.
+    return {
+      userId: user.id,
+      email: user.email,
+      username: user.username,
+    };
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async findById(userId: string) {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+  }
+
   async signup(email: string, username: string, password: string) {
+    //TLDR; 암호화된 비밀번호 저장(saltRounds 10: salt붙여 진행하는 해시 레벨을 의미)
+    const hashedPassword = await bcrypt.hash(password, 10);
     return this.prisma.user.create({
       data: {
         email,
         username,
-        password,
+        password: hashedPassword,
       },
     });
   }
 
+  //TLDR; 입력받은 비밀번호"와 "저장된 해시 비밀번호" 비교검증
   async login(email: string, password: string) {
-    return this.prisma.user.findFirst({
-      where: {
-        email,
-        password,
-      },
+    const user = await this.prisma.user.findFirst({
+      where: { email },
     });
+
+    if (!user) return null;
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) return null;
+
+    return user;
   }
 }
