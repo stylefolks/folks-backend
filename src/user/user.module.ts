ff0c77bca302821027017a98b032ac2b18560463
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-06-30 22:12:54 +0900

    Add follow service and endpoints

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { FollowService } from './follow.service';
+import { FollowController } from './follow.controller';
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController],
+  providers: [UserService, FollowService],
+  controllers: [UserController, FollowController],
 })
 export class UserModule {}
