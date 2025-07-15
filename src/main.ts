62b981ce61e113e6d261af333a13bbe34aa387d0
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-07-16 08:51:17 +0900

    feat: add swagger setup and docs (#45)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
 
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Folks API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document, { jsonDocumentUrl: 'api-json' });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
