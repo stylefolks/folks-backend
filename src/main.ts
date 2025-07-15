45d2a316d5182f270a8bad19ebf6901b1ef367b7
socar-arnold <arnold@socar.kr>
2025-07-15 17:10:17 +0900

    feat: cors enable

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { GlobalExceptionFilter } from './common/filters/global-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'], // 프론트엔드 URL
+    credentials: true, // 쿠키 등 인증 정보 허용 시
+  });
+
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 4000);
