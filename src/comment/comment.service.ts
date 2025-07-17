6f5b5ef0e0317abd840738d046059f5d0ee92679
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-07-17 16:34:39 +0900

    Return post detail comment on update (#54)

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -2,6 +2,7 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { PostDetailCommentDto } from 'src/post/dto/post-detail-comment.dto';
 
 @Injectable()
 export class CommentService {
@@ -29,16 +30,30 @@ export class CommentService {
     });
   }
 
-  async updateComment(id: string, dto: UpdateCommentDto, userId: string) {
+  async updateComment(
+    id: string,
+    dto: UpdateCommentDto,
+    userId: string,
+  ): Promise<PostDetailCommentDto> {
     const comment = await this.prisma.comment.findUnique({ where: { id } });
     if (!comment || comment.authorId !== userId) {
       throw new HttpException('Comment not found', HttpStatus.NOT_FOUND);
     }
 
-    return this.prisma.comment.update({
+    const updated = await this.prisma.comment.update({
       where: { id },
       data: dto,
+      select: {
+        id: true,
+        content: true,
+        createdAt: true,
+        author: {
+          select: { id: true, username: true },
+        },
+      },
     });
+
+    return { ...updated, createdAt: updated.createdAt.toISOString() };
   }
 
   async deleteComment(id: string, userId: string) {
