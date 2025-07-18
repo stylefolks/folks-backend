a21b3905af9eca009dd952bca6954408322c9c72
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-07-18 16:48:28 +0900

    refactor auth to use refresh token cookie

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors({
     origin: ['http://localhost:3000'], // 프론트엔드 URL
     credentials: true, // 쿠키 등 인증 정보 허용 시
